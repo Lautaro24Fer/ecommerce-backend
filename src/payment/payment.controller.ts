0c6d5843f7b3e9468615a209d3b1907cd68b010a
lautaro <lautaroleonelfernandez3@gmail.com>
2024-10-12 15:14:31 -0300

    Guardando el token en el cliente, falta eliminar la tabla en la db y testear

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get, Post, Body, HttpStatus, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpStatus, Req, Res, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { IOrderStatus, IPaymentPreferenceResponse, PaymentPreferenceRequestDto } from './dto/preference-payment';
+import { IOrderStatus, IPaymentPreferenceResponse, ITokenReq, PaymentPreferenceRequestDto } from './dto/preference-payment';
 import { Request, Response } from 'express';
+import { PaymentGuard } from './payment.guard';
 
 @ApiTags('Payments')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @ApiOperation({
+    summary: 'Get the tokens for made the operations with openpay'
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The token was created succesfully'
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'The token was not created'
+  })
+  @Get('/token')
+  async getTokens(@Req() req: Request, @Res() res: Response){
+    
+    const token: string = await this.paymentService.getTokensFromAPI();
+    res.cookie('openpay_token', token, {
+      maxAge: 1000 * 60 * 10, 
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    })
+    return res.status(201).json({ status: true, message: "The token was created ans saved succesfully" })
+  }
 
   @ApiOperation({
     summary: 'Creation of the payment preference'
@@ -20,10 +45,16 @@ export class PaymentController {
     status: HttpStatus.BAD_REQUEST,
     description: 'Error creating payment preference'
   })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized operation. Need tokens for make a payment preference'
+  })
+  @UseGuards(PaymentGuard)
   @Post('/preference')
   async createPaymentPreference(@Body() paymentPreference: PaymentPreferenceRequestDto, @Req() req: Request, @Res() res: Response ){
     console.log("______ ESTE ES EL CONTROLSDOR ________")
-    const paymentPreferenceCreated: IPaymentPreferenceResponse = await this.paymentService.createPaymentPreference(paymentPreference); 
+    const token = req.cookies['openpay_token'];
+    const paymentPreferenceCreated: IPaymentPreferenceResponse = await this.paymentService.createPaymentPreference(paymentPreference, token); 
     console.log("payment preference created")
     console.log(JSON.stringify(paymentPreferenceCreated, null , 2));
     const location: string = paymentPreferenceCreated.data.id;
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -16,11 +16,10 @@ export class PaymentService {
 		@InjectRepository(Payment) private readonly paymentRepository: Repository<Payment>, 
 		private readonly configService: ConfigService,
 		private readonly jwtService: JwtService,
-		private readonly authService: AuthService	 
 	) {}
 
   // OpenPay
-	async getTokensFromAPI(): Promise<Payment>{
+	async getTokensFromAPI(): Promise<string>{
 
 		const data = {
 			grant_type: "client_credentials",
@@ -37,7 +36,7 @@ export class PaymentService {
 				body: JSON.stringify(data)
 		}
 
-		const apiToken = await fetch(`${this.configService.get<string>('OPENPAY_AUTH_PROD')}/oauth/token`, fetchOptions)
+		const apiToken: ITokenReq = await fetch(`${this.configService.get<string>('OPENPAY_AUTH_PROD')}/oauth/token`, fetchOptions)
 		.then(response => response.json())
 		.then(data => data)
 		.catch(error => {
@@ -52,141 +51,17 @@ export class PaymentService {
 			console.log("getTokenFromAPI --) esfalso")
 		}
 
-		const tokenSaved: Payment = await this.saveTokenOnDb(apiToken)
-
-		return tokenSaved;
-	}
-
-	async saveTokenOnDb(token: ITokenReq): Promise<Payment> {
-
 		try{
 
-			if(!token.refresh_token) {
-				token.refresh_token = "";
-			}
-			// const formatedDate: string = (new Date(Number(token.expires_in) * 1000) ).toLocaleString();
-			const tokenSetted: any = { ...token  };
-			const prevToken: Payment = (await this.paymentRepository.find())[0];
-			if(prevToken){
-				await this.paymentRepository.remove(prevToken);
-			}
-			const tokenSaved: Payment = await this.paymentRepository.save(tokenSetted);
-			return tokenSaved;
+			const tokenJwt: string = await this.jwtService.signAsync(apiToken, { secret: this.configService.get<string>('OPENPAY_JWT_SECRET') })
+			return tokenJwt;
 		}
 		catch(error){
-			console.error(error)
-			throw new BadRequestException({ error: "error saving the token" })
+			throw new BadRequestException({ message: error })
 		}
 	}
 
-	jwtIsExpired(token: Payment): boolean {
-
-		console.log("\n\n === VERIFICACIÓN DEL ESTADO DE EXPIRACIÓN DEL TOKEN ACTUAL ===\n\n");
-		console.log("jwtIsExpired --) 1) Fecha del token");
-		console.log(token.expires_in);
-		console.log("jwtIsExpired --) 2) Fecha formateada a tipo date");
-		const expirationDate = new Date(Number(token.expires_in) * 1000);
-		console.log(expirationDate)
-		console.log("jwtIsExpired --) 3) Fecha actual en tipo date");
-		const currentDate = new Date();
-		console.log(currentDate)
-		console.log("jwtIsExpired --) 4) resultado final de la comparación (el token está vencido?)");
-		console.log(currentDate > expirationDate);
-		return currentDate > expirationDate;
-	}
-
-	async refreshJwtExpired(token: Payment): Promise<Payment> {
-
-		const data = {
-			grant_type: "refresh_token",
-			client_id: this.configService.get<string>('OPENPAY_CLIENT_ID'),
-			client_secret: this.configService.get<string>('OPENPAY_CLIENT_SECRET'),
-			refresh_token: token.refresh_token
-		}
-		
-		const fetchOptions = {
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(data)
-		}
-
-		const tokenRefreshed: ITokenReq = await fetch(`${this.configService.get<string>('OPENPAY_AUTH_DEV')}/oauth/token`, fetchOptions)
-		.then(response => response.json())
-		.then(data => data)
-		.catch(error => {
-			console.error(error)
-			throw new BadRequestException({ error: "error refrishing the token"	 })
-		});
-
-		const tokenSaved: Payment = await this.saveTokenOnDb(tokenRefreshed);
-		return tokenSaved;
-
-	}
-
-	async getTokensFromDatabase(): Promise<Payment> {
-		console.log("\n\n== Recuperación de los tokens de la base de datos ==\n\n");
-		console.log("getTokensFromDatabase --) 2) Recuperar los tokens mediante el find")
-		const dbToken: Payment = (await this.paymentRepository.find())[0];
-
-		console.log("\n\n ===== FIND COMPLETO DE TOKENS ===== \n\n")
-
-		const allTokens: Payment[] = await this.paymentRepository.find();
-		console.log(allTokens);
-
-		console.log("\n\n ===== FIND COMPLETO DE TOKENS ===== \n\n")
-
-		console.log("getTokensFromDatabase --) 3) __Token recuperado de la base de datos__")
-		console.log(dbToken)
-
-		if(!dbToken ) {
-			console.log("\ngetTokensFromDatabase --) 3.1) No se encontró entonces se pedirán directamente desde la api")
-			const tokenSaved: Payment = await this.getTokensFromAPI();
-			console.log("getTokensFromDatabase --) 3.2) __Token recuperado desde la API__")
-			console.log(tokenSaved)
-			return tokenSaved; 
-		}
-
-		if(this.jwtIsExpired(dbToken)){
-			console.log("\ngetTokensFromDatabase --) 4.1) El token existe en la db pero está expirado")
-
-			if(dbToken.refresh_token !== "") {
-				console.log("Se refrescará token mediante refreshToken")
-				const tokenRefreshed: Payment = await this.refreshJwtExpired(dbToken);
-				console.log("\ngetTokensFromDatabase --) 4.2) __token refrescado__");
-				console.log(tokenRefreshed)
-				return tokenRefreshed
-			}
-			else {
-				console.log("Se refrescará token un nuevo access_token de la api")
-				const tokenRefreshed: Payment = await this.getTokensFromAPI();
-				console.log("\ngetTokensFromDatabase --) 4.2) __token refrescado__");
-				console.log(tokenRefreshed)
-				return tokenRefreshed
-			}
-
-
-		}
-
-		console.log("\n\n getTokensFromDatabase --) 4.5) El token no está vencido! \n\n")
-
-		console.log("getTokensFromDatabase --) 5) Token retornado con éxito... \n\n")
-		return dbToken;
-	}
-
-	async createPaymentPreference(orderData: PaymentPreferenceRequestDto): Promise<IPaymentPreferenceResponse> {
-		console.log(`== Creación de la intención de pago ==\n\n`);
-
-		console.log("createPaymentIntent --) .5) data de la order entrante");
-		console.log(orderData)
-		console.log(JSON.stringify(orderData, null , 2));
-
-		console.log("createPaymentIntent --) 1) Recuperar tokens de la base de datos")
-		const token: Payment = await this.getTokensFromDatabase();
-		
-		console.log("createPaymentIntent --) 2) __Tokens__\n")
-		console.log(token);
+	async createPaymentPreference(orderData: PaymentPreferenceRequestDto, token: ITokenReq): Promise<IPaymentPreferenceResponse> {
 
 		const bodyData: PaymentPreferenceRequestDto = {
 			...orderData
@@ -224,7 +99,7 @@ export class PaymentService {
 
 		console.log("====== SOLICITUD DEL ESTADO DE LA ORDEN ======\n\n")
 		
-		const token: Payment = await this.getTokensFromDatabase();
+		const token = { access_token: "" } // OBJETO TEMPORAL PARA EVITAR ERRRORES DE SINTAXIS
 
 		console.log("getOrderStatus --) 1) token que se usará");
 		console.log(token);
@@ -232,7 +107,7 @@ export class PaymentService {
 		const fetchOptions = {
 			method: 'GET',
 			headers: {
-				'Content-Type': 'application/vnd.api+json',
+				'Content-Type': 'application/vnd.api+json',	
 				'Accept': 'application/vnd.api+json',
 				'Authorization': `Bearer ${token.access_token}`
 			}
